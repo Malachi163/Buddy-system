e7ea86a5f96fa5472cb7ba64f2758d775b8e64f9
Malachi163 <Samuellee240@gmail.com>
2019-10-21 09:38:52 -0400

    Add files via upload

--- node.cpp
+++ node.cpp
@@ -1,97 +1,214 @@
 #include "node.h"
 
-#include <string>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <math.h>
 
-using namespace std;
+
 
 
 int main() {
-	
-	
+
+
 	char response = 'y';
-	//int *availableList[9];
-	//int *allocatedList[9];
+
+	
+	Node<int>* allocatedList[9] = { NULL };
+	Node<int>* freeList[9] = { NULL };
+	freeList[8] = new Node<int>(256);
+	//allocatedList[8] = new Node<int>(NULL);
+	int fileSize = 0;
+	
 	
 
-	Node<int>* availableList[9];
-	Node<int> *allocatedList[9];
-	//void * freeList[9];
-	int size, s = 256;
-	size = s; //size of main memory
-	int x = ceil(log(s) / log(2)); //give us all powers of 2
 
-	//for (int i = 1; i < 9; i++)
-	//{
-	//	availableList[i] = new Node<int>(x + 1);
-	//		cout << availableList[i] << endl;
-	//}
-	//
-	//availableList = new Node availableList[x + 1];
-	for (int i = 1; i < 9; i++)
-{
-	//availableList[i] = new Node<int>[x + 1];
-		cout << availableList[i] << endl;
-		//cout << freeList[i] << endl;
-}
 	
 
-	//availableList = new Node[x + 1];
-
-	//THis is for deallocation, I will implement it later on
-	/*for (int i = 0; i < some_num; i++)
-	{
-		delete array[i];
-	}
-	delete[] array;*/
 	
-
 	while (response == 'y')
 	{
+
+		cout << "allocated blocks ---> " << endl;;
+		for (int i = 1; i < 9; i++) {
+			Node<int>* temp;
+			for (temp = allocatedList[i]; temp != NULL; temp = temp->getNext()) {
+				
+				cout << temp->getData() << " at index " << i  << endl;
+				
+			}
+		}
+
+		
+		/*for (int i = 1; i < 9; i++) {
+			if (freeList[i] != NULL) {
+				Node<int>* temp;
+				for (temp = freeList[i]; temp != NULL; temp = temp->getNext()) {
+					cout << "Free List: " << temp->getData() << " at index: " << i << endl;
+
+				}
+			}
+		}*/
+
+		
+
 		char input;
 
-		std::cout << "1. Allocates memory" << endl;
+		cout << "1. Allocates memory" << endl;
 		cout << "2. Deallocates memory" << endl;
 		cout << "3. exits the program" << endl;
 
 		cout << "What choice would you like to make: ";
-		cin >> input;
-	
-		int fileSize = 0;
+		std::cin >> input;
+
+		
+		Node<int>* ptr = NULL;
 
 		switch (input)
 		{
 		case '1':
 
 			cout << "Input size of file: ";
-			cin >> fileSize;
+			std::cin >> fileSize;
 			cout << endl << "You want to allocate " << fileSize << " kb" << endl;
+			int i, k;
 
 			if (fileSize > 256)
 			{
 				cout << "I'm sorry but the maximum file size you can store is 256 kb" << endl;
 			}
 			else {
-			cout << endl << "I am now allocating memory " << endl;
-		}
-			
 
 
-			break;
+
+				cout << endl << "I am now allocating memory " << endl;
+
+
+				if (fileSize <= 2) {
+					k = 1;
+
+				}
+
+				else if (fileSize <= 4) {
+					k = 2;
+
+				}
+
+				else if (fileSize <= 8) {
+					k = 3;
+
+				}
+
+				else if (fileSize <= 16) {
+					k = 4;
+
+				}
+
+				else if (fileSize <= 32) {
+					k = 5;
+
+				}
+				else if (fileSize <= 64) {
+
+					k = 6;
+				}
+
+				else if (fileSize <= 128) {
+
+
+					k = 7;
+
+				}
+
+
+
+				else if (fileSize <= 256) {
+					k = 8;
+
+				}
+
+				for (i = k; i < 9; ) {
+					if (freeList[i] != NULL) {
+
+						//if the block is too big, iteratively break into into two halves until you get a block just right
+						Node<int>* temp = freeList[i];
+						if (i > k) { // then break the blocks
+							freeList[i] = temp->getNext();
+							temp->setNext(NULL);
+
+							while (temp->getData() > pow(2, k)) {
+								Node<int>* temp2 = new Node<int>(temp->getData() / 2);
+								temp->setData(temp->getData() / 2);
+
+								//insert temp2 at end of list of corresponding section of freeList[i-1]
+								Node<int>* current;
+								if (freeList[i - 1] == NULL) {
+									freeList[i - 1] = temp2;
+								}
+								else {
+									for (current = freeList[i - 1]; current->getNext() != NULL; current = current->getNext());
+									current->setNext(temp2);
+								}
+								i--;
+							}
+						}
+						Node<int>* current;
+						if (allocatedList[i] == NULL) {
+							allocatedList[i] = temp;
+						}
+						else {
+							for (current = allocatedList[i]; current->getNext() != NULL; current = current->getNext());
+							current->setNext(temp);
+						}
+						break;
+					}
+					i++;
+
+				}
+
+
+
+				break;
 
 		case '2':
+			int d;
+				
+
+			for (int i = 1; i < 9; i++)
+			{
+				if (allocatedList[i] != NULL)
+				{
+					cout << endl << "At index " << i << ": " << allocatedList[i]->getData() << endl;
+					
+				}
+
+				else {
+					cout << endl << "Nothing at " << i << endl;
+				}
+			}
+
+			
+			cout << "What block would you like to delete? (choose index) " << endl;
+
+			cin >> d;
+			
+			for (int i = 1; i < 9; i++)
+			{
+				if (allocatedList[i] != NULL)
+				{
+					
+					delete allocatedList[d];
+					allocatedList[d] = NULL;
+					
+				}
+
+			}
+
 			cout << "I am now deallocating memory " << endl;
+			//std::system("cls");
 
 			break;
 
 		case '3':
 
 			cout << "Exit the program" << endl;
-			exit(1);
+			std::exit(1);
 
 			break;
 
@@ -99,15 +216,18 @@ int main() {
 			cout << "Something" << endl;
 
 
-		}
+			}
 
-		
-		cout << "Would you like to continue? (y/n) " << endl;
-		cin >> response;
-		system("cls");
+
+			cout << "Would you like to continue? (y/n) " << endl;
+			std::cin >> response;
+			
+		}
 	}
 
-	system("pause");
+		std::system("pause");
 
-	return 0;
-}
+		return 0;
+	}
+
+
--- node.h
+++ node.h
@@ -2,22 +2,23 @@
 #ifndef NODE_H
 #define NODE_H
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <queue>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 template <class Object>
 class Node {
 public:
-	Node<int>* availableList[9];
-	Node(Object initdata,Object b ) {
+	//Node<int>* availableList[9];
+	//Node(){}
+	Node(Object initdata) {
 		data = initdata; //the initialized data is set as the head
 		next = NULL; //the next node is set as NULL, as there is no next node yet.
 	}
 
 	Object getData() { //function that return data of a given node.
+
 		return data;
 	}
 
@@ -29,40 +30,19 @@ public:
 		data = newData;
 	}
 
+	
 	void setNext(Node *newnext) {
 		next = newnext;
 	}
 
-	Node(Object s) {
-		int size = s;
-		int x = ceil(log(s) / log(2)); //give us all powers of 2
-		
+	
 
-		availableList = new Node[x + 1];
-
-		for (int i = 0; i <= x; i++)
-		{
-			availableList[i] = new Node<>();
-		}
-
-		//availableList[x].push_back(new getNext(0, size - 1));
-	}
-
-	void allocate(Object s)
-	{
-		Node<int> *allocatedList[9];
-		int x = ceil(log(s) / log(2)); //give us all powers of 2
-		int i = 0;
-		Node temp = NULL;
-
-		if (availableList[x].size() > 0)
-		{
-			temp = 
-		}
-	}
+	
 private:
 	Object data; //data in the beginning node
 	Node *next; //pointer to the next node
 };
 
-#endif // !
+
+
+#endif // 
